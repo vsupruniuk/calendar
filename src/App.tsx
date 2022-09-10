61ba73b327d36340627a97346b106ad8d4d86329
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-09-10 14:12:20 +0300

    Small improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Calendar } from './components/Сalendar';
 import { Header } from './components/Header';
-import { useDate } from './hooks/useDate';
+import { Route, Routes, useSearchParams } from 'react-router-dom';
 
 export const App: React.FC = () => {
   const getActualDate = (): [number, number] => {
@@ -11,32 +11,34 @@ export const App: React.FC = () => {
     return [year, month];
   };
 
-  const [
-    year, month, setDate
-  ] = useDate(getActualDate()[0], getActualDate()[1]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const date = searchParams.get('date')
+    || `${getActualDate()[0]}-${getActualDate()[1]}`;
+  const year = Number(date.split('-')[0]);
+  const month = Number(date.split('-')[1]);
 
   const changeDate = (action: string): void => {
     switch (action) {
       case 'increase':
         if (month === 12) {
-          setDate(year + 1, 1);
+          setSearchParams({ date: `${year + 1}-${1}` });
         } else {
-          setDate(year, month + 1);
+          setSearchParams({ date: `${year}-${month + 1}` });
         }
 
         break;
 
       case 'decrease':
         if (month === 1) {
-          setDate(year - 1, 12);
+          setSearchParams({ date: `${year - 1}-${12}` });
         } else {
-          setDate(year, month - 1);
+          setSearchParams({ date: `${year}-${month - 1}` });
         }
 
         break;
 
       case 'today':
-        setDate(getActualDate()[0], getActualDate()[1]);
+        setSearchParams({});
 
         break;
 
@@ -46,18 +48,25 @@ export const App: React.FC = () => {
   };
 
   const setUserDate = (date: string): void => {
-    const splitedDate = date.split('-');
-    setDate(Number(splitedDate[0]), Number(splitedDate[1]));
+    setSearchParams({ date });
   };
 
   return (
     <div className="App">
-      <Header
-        currentMonth={[year, month]}
-        changeDate={changeDate}
-        setUserDate={setUserDate}
-      />
-      <Calendar currentMonth={[year, month]} />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header
+              currentMonth={[year, month]}
+              changeDate={changeDate}
+              setUserDate={setUserDate}
+            />
+            <Calendar currentMonth={[year, month]} />
+          </>
+        } />
+      </Routes>
+
+
     </div>
   );
 };
